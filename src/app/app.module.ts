eff2ca77e3056c5bffa120f334c016894463c0d4
Ari Peralta <168041324+AriPeraltaSolis@users.noreply.github.com>
2024-09-25 17:12:24 -0600

    Find de seccion 4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeroesModule } from './heroes/heroes.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, 
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HeroesModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
